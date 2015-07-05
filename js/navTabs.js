e4a13292f9935daa30b594cbcffac16dc6b9811c
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2015-07-06 11:05:33 +1200

    deleted unused JS, tidying styling

--- js/navTabs.js
+++
@@ -1,8 +0,0 @@
-//http://getbootstrap.com/javascript/#tabs
-$(document).ready(function() {
-  $('#navTabs a').click(function (e) {
-    e.preventDefault();
-    console.log(e);
-    $(this).tab('show');
-  })
-});
