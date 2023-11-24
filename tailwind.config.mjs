00e09f5d0e1a70432490acc3562db526410ade07
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-11-24 15:38:28 +1100

    WIP nav and focus

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'bottom-ring': '0 2px 0 0 var(--tw-ring-color)'
+			}
+		},
 	},
 	plugins: [],
 }
