be842d1dde1ad02ff5e7906436434c41f039fbce
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-01 16:28:46 +1100

    update rss config

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'iainkirkpatrick.com';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = "iain kirkpatrick's personal site";
