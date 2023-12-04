f2ef27d9c890826e891b416061aeeb0b43680aa0
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-04 17:35:46 +1100

    logs

--- functions/ask.ts
+++ functions/ask.ts
@@ -5,11 +5,11 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-  console.log(JSON.stringify(context.request.body, null, 2))
+  console.log('logging:', JSON.stringify(context.request.body, null, 2))
   const ai = new Ai(context.env.AI);
 
   // const input = { prompt: "What is the origin of the phrase Hello, World" }
-  const input = { prompt: context.request.body }
+  const input = { prompt: context.request.body.question }
 
   const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', input)
 
