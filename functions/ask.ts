e70b14587e45fbcb75ce210e99cd4c659c91aa49
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-04 17:52:46 +1100

    logs

--- functions/ask.ts
+++ functions/ask.ts
@@ -6,6 +6,7 @@ interface Env {
 
 export const onRequest: PagesFunction<Env> = async (context) => {
   console.log('logging:', JSON.stringify(context.request.body, null, 2))
+  console.log('data:', JSON.stringify(context.data, null, 2))
   const ai = new Ai(context.env.AI);
 
   // const input = { prompt: "What is the origin of the phrase Hello, World" }
