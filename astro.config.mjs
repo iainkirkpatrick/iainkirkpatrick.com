19f95bc0e3a4f27f3b5c6f91d333f34646ef790c
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-06 19:02:10 +1100

    WIP partials with htmx, upgrade Astro 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-// import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-// TODO: cloudflare adapter not building, can use advanced mode when it does (so that worker can be run locally)
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://iainkirkpatrick.com',
+  site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  // output: 'server',
-  // adapter: cloudflare()
+  output: "hybrid",
+  adapter: cloudflare()
 });
