24309838ddeeb9412722df56b0a6372759f9cd17
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-06 16:24:03 +1100

    undo local worker changes for now, astro/cloudflare not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+// import cloudflare from '@astrojs/cloudflare';
 
+// TODO: cloudflare adapter not building, can use advanced mode when it does (so that worker can be run locally)
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://iainkirkpatrick.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  // output: 'server',
+  // adapter: cloudflare()
 });
