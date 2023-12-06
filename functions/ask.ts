e2a4ca4119af1f70760f0d15ea43a922c833009d
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-06 14:58:32 +1100

    fix up TS for ask

--- functions/ask.ts
+++ functions/ask.ts
@@ -1,13 +1,14 @@
 import { Ai } from '@cloudflare/ai'
-import types from '@cloudflare/workers-types'
+import { EventContext } from '@cloudflare/workers-types'
 
-export async function onRequestPost (context) {
+interface Env {
+  AI: any
+}
+
+export async function onRequestPost (context: EventContext<Env, '', {}>) {
   try {
-    const data = await context.request.json()
+    const input: { prompt: string } = await context.request.json()
     const ai = new Ai(context.env.AI);
-
-    // const input = { prompt: "What is the origin of the phrase Hello, World" }
-    const input = { prompt: data.question }
 
     const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', input)
 
