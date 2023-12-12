885a09bef31beadbfa08a60df6de9ea30b0a117f
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-12 14:34:08 +1100

    WIP testing streaming inference

--- _worker.ts
+++ _worker.ts
@@ -11,16 +11,41 @@ export default {
 		const url = new URL(request.url)
 
 		if (url.pathname === '/ask') {
-  		try {
-				const input: { prompt: string } = await request.json()
-				const ai = new Ai(env.AI);
+			// if (request.method === 'OPTIONS') {
+      //   return new Response(null, {
+      //     status: 204,
+      //     headers: {
+      //       'Access-Control-Allow-Credentials': 'true',
+      //       'Access-Control-Allow-Methods': 'GET',
+      //       'Access-Control-Allow-Origin': 'http://localhost:3000',
+      //       'Access-Control-Allow-Headers': 'Content-Type'
+      //     }
+      //   });
+    	// } else {
+				try {
+					const ai = new Ai(env.AI);
+					const { searchParams } = new URL(request.url)
+					const input = searchParams.get('input')
 
-				const { response } = await ai.run('@cf/meta/llama-2-7b-chat-int8', input)
+					if (!input) {
+						throw new Error('No input provided')
+					} else {
+						const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+							prompt: input,
+							stream: true,
+						})
 
-				return Response.json({ answer: response });
-			} catch (error: any) {
-    		return new Response(error.message || error.toString(), { status: 500 });
-			}
+
+						return new Response(
+							response,
+							{ headers: { "content-type": "text/event-stream" } }
+						);
+					}
+				} catch (error: any) {
+					console.log({ error })
+					return new Response(error.message || error.toString(), { status: 500 });
+				}
+			// }
 		} else if (url.pathname === '/embed') {
 			try {
 				const input: { text: string | Array<string> } = await request.json()
