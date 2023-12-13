53e94fdee700c4a1f47ae746892d7fdfc1f265d0
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-13 13:27:18 +1100

    remove old code

--- functions/ask.ts
+++ functions/ask.ts
@@ -6,16 +6,6 @@ interface Env {
 }
 
 export async function onRequest (context: EventContext<Env, '', {}>) {
-  // try {
-  //   const input: { prompt: string } = await context.request.json()
-  //   const ai = new Ai(context.env.AI);
-
-  //   const { response } = await ai.run('@cf/meta/llama-2-7b-chat-int8', input)
-
-  //   return Response.json({ answer: response });
-  // } catch (error: any) {
-  //   return new Response(error.message || error.toString(), { status: 500 });
-  // }
   try {
     const ai = new Ai(context.env.AI);
     const { searchParams } = new URL(context.request.url)
