f45a7adc071efbd200ac25d776db702411e381cf
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2023-12-13 12:10:22 +1100

    update ask function

--- functions/ask.ts
+++ functions/ask.ts
@@ -5,15 +5,38 @@ interface Env {
   AI: any
 }
 
-export async function onRequestPost (context: EventContext<Env, '', {}>) {
+export async function onRequest (context: EventContext<Env, '', {}>) {
   try {
-    const input: { prompt: string } = await context.request.json()
+  //   const input: { prompt: string } = await context.request.json()
+  //   const ai = new Ai(context.env.AI);
+
+  //   const { response } = await ai.run('@cf/meta/llama-2-7b-chat-int8', input)
+
+  //   return Response.json({ answer: response });
+  // } catch (error: any) {
+  //   return new Response(error.message || error.toString(), { status: 500 });
+  // }
+  try {
     const ai = new Ai(context.env.AI);
+    const { searchParams } = new URL(context.request.url)
+    const input = searchParams.get('input')
 
-    const { response } = await ai.run('@cf/meta/llama-2-7b-chat-int8', input)
+    if (!input) {
+      throw new Error('No input provided')
+    } else {
+      const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+        prompt: input,
+        stream: true,
+      })
 
-    return Response.json({ answer: response });
+
+      return new Response(
+        response,
+        { headers: { "content-type": "text/event-stream" } }
+      );
+    }
   } catch (error: any) {
+    console.log({ error })
     return new Response(error.message || error.toString(), { status: 500 });
   }
 }
