2eebc8f89e122e2138285cec021360e10d9b5627
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2024-02-09 11:17:40 +1100

    shuffle around for clearer separation of pages and workers

--- workers/index.ts
+++ workers/index.ts
@@ -1,19 +1,8 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import type { ExecutionContext } from '@cloudflare/workers-types';
 
 import type { Env } from '../src/types/env';
 
-import { onRequest as ask } from '../functions/ask';
-import { onRequestPost as embed } from '../functions/embed';
+import { onRequest as ask } from './ask';
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -34,9 +23,6 @@ export default {
 			if (reqUrl.pathname === '/ask') {
 				// @ts-ignore: not quite the right type for ask, but has enough overlap
 				return ask({ request, env, ctx }, corsHeaders)
-			} else if (reqUrl.pathname === '/embed') {
-				// @ts-ignore: not quite the right type for ask, but has enough overlap
-				return embed({ request, env, ctx })
 			} else {
 				return new Response('no matching route!');
 			}
